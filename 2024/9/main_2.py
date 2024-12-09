416f77552ca827577bcfe9af0e2bbd3e1e2464a3
Malo Kerrand <malo.kerrand@fre.fr>
2024-12-09 21:22:07 +0100

    9.2

--- 2024/9/main_2.py
+++ 2024/9/main_2.py
@@ -1,26 +1,36 @@
 from copy import deepcopy
 
 
-def insert(file_id: str, file_size: int, disk: list[tuple[str, int]]):
+def empty(file_id: str, disk: list[tuple[str, int]]) -> None:
+    for index, (space_id, space) in enumerate(disk):
+        if space_id == file_id:
+            disk[index] = (".", space)
+
+
+def insert(file_id: str, file_size: int, disk: list[tuple[str, int]]) -> list[tuple[str, int]]:
     final_disk = deepcopy(disk)
     for index, (space_id, space) in enumerate(disk):
+        if space_id == file_id:
+            return disk
         if space_id != ".":
             continue
         if space < file_size:
             continue
 
+        empty(file_id=file_id, disk=final_disk)
+
         if space == file_size:
             final_disk[index] = (file_id, file_size)
         else:
             final_disk[index] = (space_id, space - file_size)
-            final_disk = final_disk[:index] + [(file_id, file_size)] + final_disk[index + 1 :]
+            final_disk = final_disk[:index] + [(file_id, file_size)] + final_disk[index:]
 
         return final_disk
     return final_disk
 
 
 def main():
-    with open("input_fake", "r") as f:
+    with open("input", "r") as f:
         disk_map: str = f.read()
 
     # (id, space)
@@ -31,17 +41,19 @@ def main():
         else:
             original_disk.append((".", int(nb)))
 
-    print(original_disk)
-    # (id, space)
-    disk: list[tuple[str, int]] = deepcopy(original_disk)
-    final_disk: list[tuple[str, int]] = []
-    while disk:
-        file_id, file_size = disk.pop()
+    final_disk: list[tuple[str, int]] = deepcopy(original_disk)
+    total_size = len(original_disk)
+    current = 0
+    for file_id, file_size in reversed(original_disk):
+        current += 1
+        print(f"{100 * current / total_size: .2f} %")
+        if file_id == ".":
+            continue
         final_disk = insert(file_id=file_id, file_size=file_size, disk=final_disk)
-    print(final_disk)
 
-    final_disk = [file_id for file_id, file_size in final_disk for _ in range(file_size)]
-    print(sum(file_size * int(file_id) for file_id, file_size in final_disk if file_id != "."))
+    final_disk_2 = [file_id for file_id, file_size in final_disk for _ in range(file_size)]
+    print("".join(final_disk_2))
+    print(sum(index * int(char) for index, char in enumerate(final_disk_2) if char != "."))
 
 
 if __name__ == "__main__":
