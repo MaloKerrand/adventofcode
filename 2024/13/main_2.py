3d11b1fe8711b6b698aebe19ed7e0a3d91834336
Malo Kerrand <malo.kerrand@fre.fr>
2024-12-13 20:35:00 +0100

    13.2

--- 2024/13/main_2.py
+++ 2024/13/main_2.py
@@ -1,3 +1,4 @@
+import math
 from dataclasses import dataclass
 from functools import cache
 
@@ -11,52 +12,23 @@ class Machine:
     Xp: int = None
     Yp: int = None
 
-    def result(self, nb_a: int, nb_b: int) -> tuple[int, int]:
-        return (nb_a * self.Xa + nb_b * self.Xb), (nb_a * self.Ya + nb_b * self.Yb)
+    def is_solution(self, nb_a: int, nb_b: int) -> bool:
+        return nb_a * self.Xa + nb_b * self.Xb == self.Xp and nb_a * self.Ya + nb_b * self.Yb == self.Yp
 
     @staticmethod
     def price(nb_a: int, nb_b: int) -> int:
         return 3 * nb_a + nb_b
 
-    @cache
-    def minimum_price(self, target_x: int = None, target_y: int = None) -> int | None:
-        if target_x is None:
-            target_x = self.Xp
-        if target_y is None:
-            target_y = self.Yp
-
-        if target_x < 0 or target_y < 0:
-            return None
-
-        if target_x == 0 and target_y == 0:
-            return 0
-
-        price_a = None
-        if target_x >= self.Xa and target_y >= self.Ya:
-            price = self.minimum_price(target_x=target_x - self.Xa, target_y=target_y - self.Ya)
-            if price is not None:
-                price_a = 3 + price
-
-        price_b = None
-        if target_x >= self.Xb and target_y >= self.Yb:
-            price = self.minimum_price(target_x=target_x - self.Xb, target_y=target_y - self.Yb)
-            if price is not None:
-                price_b = 1 + price
-
-        if price_b is None and price_a is None:
-            return None
-
-        if price_a is None:
-            return price_b
-
-        if price_b is None:
-            return price_a
-
-        return min(price_a, price_b)
+    def solution(self) -> tuple[int, int] | None:
+        nb_b: float = (self.Xa * self.Yp - self.Xp * self.Ya) / (self.Yb * self.Xa - self.Xb * self.Ya)
+        nb_a: float = (self.Xb * self.Yp - self.Xp * self.Yb) / (self.Ya * self.Xb - self.Xa * self.Yb)
+        if nb_a.is_integer() and nb_b.is_integer():
+            return int(round(nb_a)), int(round(nb_b))
+        return None
 
 
 def main():
-    with open("input_fake", "r") as f:
+    with open("input", "r") as f:
         lines: list[str] = f.read().splitlines()
     machines: list[Machine] = []
     machine = Machine()
@@ -82,7 +54,9 @@ def main():
     nb_machines = len(machines)
     for current, machine in enumerate(machines):
         print(f"{100 * current / nb_machines: .2f} %")
-        total += machine.minimum_price() or 0
+        solution = machine.solution()
+        if solution is not None:
+            total += machine.price(nb_a=solution[0], nb_b=solution[1])
 
     print(total)
 
