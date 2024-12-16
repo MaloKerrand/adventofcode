5c7deee28823de720b037a9a7d1a4020769fc335
Malo Kerrand <malo.kerrand@fre.fr>
2024-12-16 20:55:01 +0100

    16 does not works

--- 2024/16/main_1.py
+++ 2024/16/main_1.py
@@ -1,3 +1,6 @@
+import math
+from bisect import insort
+from copy import deepcopy
 from dataclasses import dataclass
 
 
@@ -7,43 +10,76 @@ class Position:
     y: int
     dx: int
     dy: int
+    cost: int
+    distance: float
+
+    def __lt__(self, other: "Position"):
+        if self.cost == other.cost:
+            return self.distance < other.distance
+        return self.cost < other.cost
 
 
-MAZE: list[str] = []
-POSITION_TO_COST: dict[Position, int] = {}
+class PriorityList:
+    def __init__(self, elements: list[Position]|None = None):
+        self.elements = elements or []
+
+    def append(self, position: Position):
+        insort(self.elements, position, key=lambda p: -p.cost)
+
+
+MAZE: list[list[str]] = []
 
 
 def main():
     global MAZE
     with open("input_fake_2", "r") as f:
-        MAZE = [line for line in f.read().splitlines()]
+        MAZE = [list(line) for line in f.read().splitlines()]
 
     start_x, start_y, end_x, end_y = get_start_end()
-    new_to_see: list[tuple[int, Position]] = [(0, Position(x=start_x, y=start_y, dx=1, dy=0))]
-    while new_to_see:
-        cost, position = new_to_see.pop()
+    priority_list = PriorityList(
+        elements=[
+            Position(
+                x=start_x,
+                y=start_y,
+                dx=1,
+                dy=0,
+                cost=0,
+                distance=math.dist((start_x, start_y), (end_x, end_y))
+            )
+        ]
+    )
+    while priority_list.elements:
+        position = priority_list.elements.pop()
+        print(priority_list.elements)
+        print(position.x, position.y)
+        show(position.x, position.y)
+        input()
         for new_dx, new_dy in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
             if new_dx == -position.dx and new_dy == -position.dy:
                 continue
             new_x = position.x + new_dx
             new_y = position.y + new_dy
-            new_position = Position(x=new_x, y=new_y, dx=new_dx, dy=new_dy)
 
             if MAZE[new_y][new_x] == "#":
                 continue
 
-            new_cost = cost + (1 if new_dx == position.dx and new_dy == position.dy else 1001)
-            if new_position in POSITION_TO_COST:
-                POSITION_TO_COST[new_position] = min(new_cost, POSITION_TO_COST[new_position])
-                continue
-            POSITION_TO_COST[new_position] = new_cost
+            new_cost = position.cost + (1 if new_dx == position.dx and new_dy == position.dy else 1001)
+            new_position = Position(
+                x=new_x,
+                y=new_y,
+                dx=new_dx,
+                dy=new_dy,
+                cost=new_cost,
+                distance=math.dist((new_x, new_y), (end_x, end_y)),
+            )
 
             if MAZE[new_y][new_x] == "E":
-                continue
-            new_to_see.append((new_cost, new_position))
+                print("FOUND IT !", new_position.cost)
+                return
 
-    for dx, dy in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
-        print(POSITION_TO_COST.get(Position(x=end_x, y=end_y, dx=dx, dy=dy)))
+            priority_list.append(new_position)
+
+    print("Not found :<")
 
 
 def get_start_end() -> tuple[int, int, int, int]:
@@ -60,6 +96,13 @@ def get_start_end() -> tuple[int, int, int, int]:
                 found = (x, y)
 
     raise Exception("No S")
+
+
+def show(x, y):
+    maze_2 = deepcopy(MAZE)
+    maze_2[y][x] = "O"
+    for line in maze_2:
+        print("".join(line))
 
 
 if __name__ == "__main__":
--- 2024/16/main_2.py
+++ 2024/16/main_2.py
@@ -1,182 +1,70 @@
 from dataclasses import dataclass
-from enum import Enum
 
 
-class Entity(Enum):
-    ROBOT = "robot"
-    WALL = "wall"
-    BOX_L = "box_l"
-    BOX_R = "box_r"
+@dataclass(frozen=True)
+class Position:
+    x: int
+    y: int
+    dx: int
+    dy: int
 
 
-@dataclass(unsafe_hash=True)
-class Warehouse:
-    robot_x: int = None
-    robot_y: int = None
-    walls: list[tuple[int, int]] = None
-    boxes: list[tuple[int, int]] = None
-
-    @staticmethod
-    def from_string(string: str) -> "Warehouse":
-        warehouse = Warehouse()
-        for y, line in enumerate(string.splitlines()):
-            for x, char in enumerate(line):
-                match char:
-                    case "@":
-                        warehouse.robot_x = 2 * x
-                        warehouse.robot_y = y
-                    case "#":
-                        if warehouse.walls is None:
-                            warehouse.walls = []
-                        warehouse.walls.append((2 * x, y))
-                        warehouse.walls.append((2 * x + 1, y))
-                    case "O":
-                        if warehouse.boxes is None:
-                            warehouse.boxes = []
-                        warehouse.boxes.append((2 * x, y))
-        warehouse.show()
-        return warehouse
-
-    def compute_step(self, dx: int, dy: int) -> None:
-        self.move(x=self.robot_x, y=self.robot_y, dx=dx, dy=dy)
-
-    def move(self, x: int, y: int, dx: int, dy: int) -> None:
-        if not self.can_move(x=x, y=y, dx=dx, dy=dy):
-            return
-
-        new_x: int = x + dx
-        new_y: int = y + dy
-        entity: Entity | None = self.get_entity(x=x, y=y)
-        match (entity, dx, dy):
-            case Entity.ROBOT, _, _:
-                self.move(x=new_x, y=new_y, dx=dx, dy=dy)
-                self.robot_x += dx
-                self.robot_y += dy
-
-            case Entity.BOX_L, 1, _:
-                self.move(x=new_x + 1, y=new_y, dx=dx, dy=dy)
-                index = self.boxes.index((x, y))
-                self.boxes[index] = (new_x, new_y)
-
-            case Entity.BOX_L, -1, _:
-                raise Exception("No possible")
-
-            case Entity.BOX_L, 0, _:
-                self.move(x=new_x, y=new_y, dx=dx, dy=dy)
-                self.move(x=new_x + 1, y=new_y, dx=dx, dy=dy)
-                index = self.boxes.index((x, y))
-                self.boxes[index] = (new_x, new_y)
-
-            case Entity.BOX_R, 1, _:
-                raise Exception("No possible")
-
-            case Entity.BOX_R, -1, _:
-                self.move(x=new_x - 1, y=new_y, dx=dx, dy=dy)
-                index = self.boxes.index((x - 1, y))
-                self.boxes[index] = (new_x - 1, new_y)
-
-            case Entity.BOX_R, 0, _:
-                self.move(x=new_x, y=new_y, dx=dx, dy=dy)
-                self.move(x=new_x - 1, y=new_y, dx=dx, dy=dy)
-                index = self.boxes.index((x - 1, y))
-                self.boxes[index] = (new_x - 1, new_y)
-
-    def can_move(self, x: int, y: int, dx: int, dy: int) -> bool:
-        new_x: int = x + dx
-        new_y: int = y + dy
-        entity: Entity | None = self.get_entity(x=x, y=y)
-        match (entity, dx, dy):
-            case Entity.WALL, _, _:
-                return False
-            case Entity.ROBOT, _, _:
-                return self.can_move(x=new_x, y=new_y, dx=dx, dy=dy)
-
-            case Entity.BOX_L, 1, _:
-                return self.can_move(x=new_x + 1, y=new_y, dx=dx, dy=dy)
-
-            case Entity.BOX_L, -1, _:
-                raise Exception("No possible")
-
-            case Entity.BOX_L, 0, _:
-                return self.can_move(x=new_x, y=new_y, dx=dx, dy=dy) and self.can_move(
-                    x=new_x + 1, y=new_y, dx=dx, dy=dy
-                )
-
-            case Entity.BOX_R, 1, _:
-                raise Exception("No possible")
-
-            case Entity.BOX_R, -1, _:
-                return self.can_move(x=new_x - 1, y=new_y, dx=dx, dy=dy)
-
-            case Entity.BOX_R, 0, _:
-                return self.can_move(x=new_x, y=new_y, dx=dx, dy=dy) and self.can_move(
-                    x=new_x - 1, y=new_y, dx=dx, dy=dy
-                )
-        return True
-
-    def get_entity(self, x: int, y: int) -> Entity | None:
-        if self.robot_x == x and self.robot_y == y:
-            return Entity.ROBOT
-
-        if (x, y) in self.walls:
-            return Entity.WALL
-
-        if (x, y) in self.boxes:
-            return Entity.BOX_L
-
-        if (x - 1, y) in self.boxes:
-            return Entity.BOX_R
-
-        return None
-
-    def show(self) -> None:
-        max_x = max(wall[0] for wall in self.walls)
-        max_y = max(wall[1] for wall in self.walls)
-        _map = [["." for _ in range(max_x + 1)] for _ in range(max_y + 1)]
-
-        _map[self.robot_y][self.robot_x] = "@"
-        for x, y in self.walls:
-            _map[y][x] = "#"
-        for x, y in self.boxes:
-            _map[y][x] = "["
-            _map[y][x + 1] = "]"
-
-        for line in _map:
-            print("".join(line))
-
-    def sum_gps(self) -> int:
-        return sum(100 * box[1] + box[0] for box in self.boxes)
-
-
-def step_to_direction(step: str) -> tuple[int, int]:
-    match step:
-        case "<":
-            return -1, 0
-        case "^":
-            return 0, -1
-        case ">":
-            return 1, 0
-        case "v":
-            return 0, 1
-    raise Exception(f"Unknown step {step!r}")
-
+MAZE: list[str] = []
+POSITION_TO_COST: dict[Position, int] = {}
+ALL_PATHS: list[list[Position]] = []
 
 def main():
-    with open("input", "r") as f:
-        content: str = f.read()
-    p1, p2 = content.split("\n\n")
-    warehouse: Warehouse = Warehouse.from_string(p1)
-    steps: str = p2.replace("\n", "")
+    global MAZE
+    with open("input_fake_2", "r") as f:
+        MAZE = [line for line in f.read().splitlines()]
 
-    print(steps)
-    for step in steps:
-        dx, dy = step_to_direction(step)
-        warehouse.compute_step(dx=dx, dy=dy)
-        # print(step)
-        # warehouse.show()
-    warehouse.show()
+    start_x, start_y, end_x, end_y = get_start_end()
+    new_to_see: list[tuple[int, Position]] = [(0, Position(x=start_x, y=start_y, dx=1, dy=0))]
+    while new_to_see:
+        cost, position = new_to_see.pop()
+        for new_dx, new_dy in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
+            if new_dx == -position.dx and new_dy == -position.dy:
+                continue
+            new_x = position.x + new_dx
+            new_y = position.y + new_dy
+            new_position = Position(x=new_x, y=new_y, dx=new_dx, dy=new_dy)
 
-    print(warehouse.sum_gps())
+            if MAZE[new_y][new_x] == "#":
+                continue
+
+            new_cost = cost + (1 if new_dx == position.dx and new_dy == position.dy else 1001)
+            if new_position in POSITION_TO_COST:
+                POSITION_TO_COST[new_position] = min(new_cost, POSITION_TO_COST[new_position])
+                continue
+            POSITION_TO_COST[new_position] = new_cost
+
+            if MAZE[new_y][new_x] == "E":
+                continue
+            new_to_see.append((new_cost, new_position))
+
+    print("E")
+    for dx, dy in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
+        print(POSITION_TO_COST.get(Position(x=end_x, y=end_y, dx=dx, dy=dy)))
+
+    print("****")
+    for dx, dy in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
+        print(POSITION_TO_COST.get(Position(x=end_x - 1, y=end_y, dx=dx, dy=dy)))
+
+
+def get_start_end() -> tuple[int, int, int, int]:
+    found: tuple[int, int] | None = None
+    for y, line in enumerate(MAZE):
+        for x, char in enumerate(line):
+            if char == "S":
+                if found:
+                    return x, y, found[0], found[1]
+                found = (x, y)
+            if char == "E":
+                if found:
+                    return found[0], found[1], x, y
+                found = (x, y)
+
+    raise Exception("No S")
 
 
 if __name__ == "__main__":
