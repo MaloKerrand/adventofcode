c6077080b5f1c31c0b0fb9c4f442a5284f4021f6
Malo Kerrand <malo.kerrand@fre.fr>
2024-12-17 21:33:15 +0100

    17

--- 2024/17/main_1.py
+++ 2024/17/main_1.py
@@ -1,16 +1,4 @@
-import math
-from bisect import insort
-from copy import deepcopy
-from dataclasses import dataclass
-
-register_a = 0
-register_b = 0
-register_c = 0
-instruction = 0
-output: list[int] = []
-
-
-def operand_to_value(operand: int) -> int:
+def operand_to_value(operand: int, register_a: int, register_b: int, register_c: int) -> int | None:
     match operand:
         case 0 | 1 | 2 | 3:
             return operand
@@ -20,38 +8,58 @@ def operand_to_value(operand: int) -> int:
             return register_b
         case 6:
             return register_c
-    raise Exception("opcode not supported")
+    return None
 
 
-def opcode_to_value(opcode: int, literal_operand: int) -> None:
-    global register_a, register_b, register_c, instruction
-    combo_operand_value = operand_to_value(literal_operand)
-    match opcode:
-        case 0:  # adv
-            register_a = register_a // (2**combo_operand_value)
-        case 1:  # bxl
-            register_b = register_b ^ literal_operand
-        case 2:  # bst
-            register_b = (combo_operand_value % 8) & 0b111
-        case 3:  # jmz
-            if register_a == 0:
-                return
-            instruction += literal_operand  # -2 ?
-        case 4:  # bxc
-            register_b = register_b ^ register_c
-        case 5:  # out
-            output.append((combo_operand_value % 8) & 0b111)
-        case 6:  # bdv
-            register_b = register_a // (2**combo_operand_value)
-        case 7:  # cdv
-            register_c = register_a // (2**combo_operand_value)
+def compute(program: list[int], register_a: int,  register_b: int, register_c: int) -> list[int]:
+    output: list[int] = []
+    instruction = 0
+    while instruction < len(program):
+        opcode = program[instruction]
+        literal_operand = program[instruction + 1]
+        # print(instruction, opcode, literal_operand, register_a, register_b, register_c)
+
+        combo_operand_value: int | None = operand_to_value(
+            operand=literal_operand,
+            register_a=register_a,
+            register_b=register_b,
+            register_c=register_c
+        )
+        match opcode:
+            case 0:  # adv
+                register_a = register_a // (2 ** combo_operand_value)
+            case 1:  # bxl
+                register_b = register_b ^ literal_operand
+            case 2:  # bst
+                register_b = (combo_operand_value % 8) & 0b111
+            case 3:  # jmz
+                if register_a != 0:
+                    instruction = literal_operand - 2  # -2 ?
+            case 4:  # bxc
+                register_b = register_b ^ register_c
+            case 5:  # out
+                output.append((combo_operand_value % 8) & 0b111)
+            case 6:  # bdv
+                register_b = register_a // (2 ** combo_operand_value)
+            case 7:  # cdv
+                register_c = register_a // (2 ** combo_operand_value)
+
+        instruction += 2
+    return output
 
 
 def main():
-    with open("input", "r") as f:
-        MAZE = [list(line) for line in f.read().splitlines()]
+    with open("input_fake", "r") as f:
+        content: list[str] = f.read().splitlines()
+
+    register_a: int = int(content[0].split(" ")[2])
+    register_b: int = int(content[1].split(" ")[2])
+    register_c: int = int(content[2].split(" ")[2])
+
+    program: list[int] = [int(e) for e in content[4].split(" ")[1].split(",")]
+    output = compute(program=program, register_a=register_a, register_b=register_b, register_c=register_c)
+    print(",".join([str(i) for i in output]))
 
 
 if __name__ == "__main__":
-    # print(timeit.timeit("main()", number=1000, globals=locals()))
     main()
--- 2024/17/main_2.py
+++ 2024/17/main_2.py
@@ -1,104 +1,108 @@
-import math
-from bisect import insort
-from copy import deepcopy
-from dataclasses import dataclass
+def operand_to_value(operand: int, register_a: int, register_b: int, register_c: int) -> int | None:
+    match operand:
+        case 0 | 1 | 2 | 3:
+            return operand
+        case 4:
+            return register_a
+        case 5:
+            return register_b
+        case 6:
+            return register_c
+    return None
 
 
-@dataclass(frozen=True)
-class Position:
-    x: int
-    y: int
-    dx: int
-    dy: int
-    cost: int
-    distance: float
+def compute(program: list[int], register_a: int,  register_b: int, register_c: int) -> list[int] | None:
+    output: list[int] = []
+    instruction = 0
+    while instruction < len(program):
+        opcode = program[instruction]
+        literal_operand = program[instruction + 1]
+        # print(instruction, opcode, literal_operand, register_a, register_b, register_c)
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y and self.dx == other.dx and self.dy == other.dy
+        combo_operand_value: int | None = operand_to_value(
+            operand=literal_operand,
+            register_a=register_a,
+            register_b=register_b,
+            register_c=register_c
+        )
+        match opcode:
+            case 0:  # adv
+                register_a = register_a // (2 ** combo_operand_value)
+            case 1:  # bxl
+                register_b = register_b ^ literal_operand
+            case 2:  # bst
+                register_b = (combo_operand_value % 8) & 0b111
+            case 3:  # jmz
+                if register_a != 0:
+                    instruction = literal_operand - 2  # -2 ?
+            case 4:  # bxc
+                register_b = register_b ^ register_c
+            case 5:  # out
+                output.append((combo_operand_value % 8) & 0b111)
+                # if output != program[:len(output)]:
+                #     return None
+            case 6:  # bdv
+                register_b = register_a // (2 ** combo_operand_value)
+            case 7:  # cdv
+                register_c = register_a // (2 ** combo_operand_value)
+
+        instruction += 2
+    return output
 
 
-class PriorityList:
-    def __init__(self, elements: list[Position] | None = None):
-        self.elements = elements or []
+def smaller_a(
+    program: list[int],
+    index: int,
+    a_list: list[int],
+    register_b: int,
+    register_c: int
+) -> list[int] | None:
+    register_a = a_list.copy()
+    if index == -1:
+        return register_a
 
-    def append(self, position: Position):
-        insort(self.elements, position, key=lambda p: -(p.cost + p.distance))
+    possibles: set[int] = set()
+    for i in range(0, 8):
+        register_a[index] = i
+        a = sum(i * 8 ** index for index, i in enumerate(register_a))
+        output = compute(program=program, register_a=a, register_b=register_b, register_c=register_c)
+        if len(output) == len(program) and output[index] == program[index]:
+            possibles.add(i)
 
-
-MAZE: list[list[str]] = []
-KNOWN_POSITIONS: list[Position] = []
+    for i in sorted(possibles):
+        register_a[index] = i
+        small_a = smaller_a(
+            program=program,
+            index=index - 1,
+            a_list=register_a,
+            register_b=register_b,
+            register_c=register_c
+        )
+        if small_a is not None:
+            return small_a
+    return None
 
 
 def main():
-    global MAZE
     with open("input", "r") as f:
-        MAZE = [list(line) for line in f.read().splitlines()]
+        content: list[str] = f.read().splitlines()
 
-    start_x, start_y, end_x, end_y = get_start_end()
-    start_position = Position(
-        x=start_x, y=start_y, dx=1, dy=0, cost=0, distance=math.dist((start_x, start_y), (end_x, end_y))
+    # register_a: int = int(content[0].split(" ")[2])
+    register_b: int = int(content[1].split(" ")[2])
+    register_c: int = int(content[2].split(" ")[2])
+
+    program: list[int] = [int(e) for e in content[4].split(" ")[1].split(",")]
+    a_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    a_list = smaller_a(
+        program=program,
+        index=len(a_list) - 1,
+        a_list=a_list,
+        register_b=register_b,
+        register_c=register_c
     )
-    priority_list = PriorityList(elements=[start_position])
-    KNOWN_POSITIONS.append(start_position)
-    while priority_list.elements:
-        position = priority_list.elements.pop()
-        for new_dx, new_dy in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
-            if new_dx == -position.dx and new_dy == -position.dy:
-                continue
-            new_x = position.x + new_dx
-            new_y = position.y + new_dy
-
-            if MAZE[new_y][new_x] == "#":
-                continue
-
-            new_cost = position.cost + (1 if new_dx == position.dx and new_dy == position.dy else 1001)
-            new_position = Position(
-                x=new_x,
-                y=new_y,
-                dx=new_dx,
-                dy=new_dy,
-                cost=new_cost,
-                distance=math.dist((new_x, new_y), (end_x, end_y)),
-            )
-
-            if new_position in KNOWN_POSITIONS:
-                continue
-            KNOWN_POSITIONS.append(new_position)
-
-            if MAZE[new_y][new_x] == "E":
-                print("FOUND IT !", new_position.cost)
-                break
-
-            priority_list.append(new_position)
-
-    print(new_position)
-    while new_position != start_position:
-        pass
-
-
-def get_start_end() -> tuple[int, int, int, int]:
-    found: tuple[int, int] | None = None
-    for y, line in enumerate(MAZE):
-        for x, char in enumerate(line):
-            if char == "S":
-                if found:
-                    return x, y, found[0], found[1]
-                found = (x, y)
-            if char == "E":
-                if found:
-                    return found[0], found[1], x, y
-                found = (x, y)
-
-    raise Exception("No S")
-
-
-def show(x, y):
-    maze_2 = deepcopy(MAZE)
-    maze_2[y][x] = "O"
-    for line in maze_2:
-        print("".join(line))
+    a = sum(i * 8 ** index for index, i in enumerate(a_list))
+    print(a)
 
 
 if __name__ == "__main__":
-    # print(timeit.timeit("main()", number=1000, globals=locals()))
     main()
