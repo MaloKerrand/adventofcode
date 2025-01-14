58d320cc91f14195ba8b432d3e3600747b95d8d5
Malo Kerrand <mkerrand@onestock-retail.com>
2025-01-14 13:47:40 +0100

    24.2

--- 2024/24/main_2.py
+++ 2024/24/main_2.py
@@ -1,120 +1,21 @@
+from dataclasses import dataclass
+
+good_keys: set[str] = set()
+
+
+@dataclass
 class Equation:
-    def __init__(self, left: "Equation | str", right: "Equation | str", op: str, name: str):
-        self.raw_left = left
-        self.raw_right = right
-        self.op = op
-        self.name = name
+    left: str
+    op: str
+    right: str
 
-    def __eq__(self, other: "Equation"):
+    def __eq__(self, other):
         if not isinstance(other, Equation):
-            return False
-
-        if self.op != other.op:
-            return False
-
-        if (self.left == other.left and self.right == other.right) or (
-            self.left == other.right and self.right == other.left
-        ):
-            return True
-
-        # Case (a and b) or (c and d) or (e and f)
-        if (
-            isinstance(self.right, str)
-            or isinstance(self.left, str)
-            or isinstance(other.right, str)
-            or isinstance(other.left, str)
-        ):
-            return False
-
-        if not (self.right.op == self.left.op == other.right.op == other.left.op):
-            return False
-
-        return False
-
-    @property
-    def left(self):
-        if isinstance(self.raw_left, str) and not self.raw_left.startswith("x") and not self.raw_left.startswith("y"):
-            self.raw_left = EQUATIONS[self.raw_left]
-        return self.raw_left
-
-    @property
-    def right(self):
-        if (
-            isinstance(self.raw_right, str)
-            and not self.raw_right.startswith("x")
-            and not self.raw_right.startswith("y")
-        ):
-            self.raw_right = EQUATIONS[self.raw_right]
-        return self.raw_right
-
-    def __str__(self):
-        return f"Equation(left={self.left!r}, right={self.right!r}, op={self.op!r})"
-
-    def __repr__(self):
-        return str(self)
-
-    def display(self, depth: int | None = None):
-        lines, *_ = self._display_aux(depth)
-        for line in lines:
-            print(line)
-
-    def _display_aux(self, depth: int | None = None):
-        """Returns list of strings, width, height, and horizontal coordinate of the root."""
-        # Depth 0
-        if depth == 0:
-            left = self.left if isinstance(self.left, str) else self.left.name
-            right = self.right if isinstance(self.right, str) else self.right.name
-            line = f"{left}-{self.op}-({self.name})-{right}"
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # No child.
-        if isinstance(self.right, str) and isinstance(self.left, str):
-            line = f"{self.left}-{self.op}-({self.name})-{self.right}"
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # Only left child.
-        if isinstance(self.right, str):
-            lines, n, p, x = self.left._display_aux(depth - 1 if depth is not None else None)
-            s = f"{self.op}-({self.name})-{self.right}"
-            u = len(s)
-            first_line = (x + 1) * " " + (n - x - 1) * "_" + s
-            second_line = x * " " + "/" + (n - x - 1 + u) * " "
-            shifted_lines = [line + u * " " for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
-
-        # Only right child.
-        if isinstance(self.left, str):
-            lines, n, p, x = self.right._display_aux(depth - 1 if depth is not None else None)
-            s = f"{self.left}-{self.op}-({self.name})"
-            u = len(s)
-            first_line = s + x * "_" + (n - x) * " "
-            second_line = (u + x) * " " + "\\" + (n - x - 1) * " "
-            shifted_lines = [u * " " + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
-        # Two children.
-        left, n, p, x = self.left._display_aux(depth - 1 if depth is not None else None)
-        right, m, q, y = self.right._display_aux(depth - 1 if depth is not None else None)
-        s = f"{self.op}-({self.name})"
-        u = len(s)
-        first_line = (x + 1) * " " + (n - x - 1) * "_" + s + y * "_" + (m - y) * " "
-        second_line = x * " " + "/" + (n - x - 1 + u + y) * " " + "\\" + (m - y - 1) * " "
-        if p < q:
-            left += [n * " "] * (q - p)
-        elif q < p:
-            right += [m * " "] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + u * " " + b for a, b in zipped_lines]
-        return lines, n + m + u, max(p, q) + 2, n + u // 2
-
-
-EQUATIONS: dict[str, Equation] = {}
+            raise TypeError(f"Cannot compare Equation object to {type(other)}")
+        return self.op == other.op and (
+            (self.left == other.left and self.right == other.right)
+            or (self.left == other.right and self.right == other.left)
+        )
 
 
 def main():
@@ -123,13 +24,191 @@ def main():
 
     _, equations_raw = content.split("\n\n")
 
+    equations: dict[str, Equation] = {}
     for equation in equations_raw.splitlines():
         n1, operation, n2, _, r = equation.split(" ")
-        EQUATIONS[r] = Equation(left=n1, right=n2, op=operation, name=r)
+        equations[r] = Equation(left=n1, op=operation, right=n2)
 
-    for n in range(4):
-        print(f"---- z{n:02d} -----")
-        EQUATIONS[f"z{n:02d}"].display(depth=2)
+    swaps = solve(equations)
+    print(",".join(sorted(s for swap in swaps for s in swap)))
+
+
+def solve(equations: dict[str, Equation]) -> list[tuple[str, str]]:
+    swaps: list[tuple[str, str]] = []
+    or_n1: str | None = None
+    xor_n1: str | None = None
+
+    for n in range(45):
+        match n:
+            case 0:
+                swaps += z_0(equations)
+                continue
+            case 1:
+                s, or_n1, xor_n1 = z_1(equations)
+                swaps += s
+            case _:
+                s, or_n1, xor_n1 = z_n(n=n, equations=equations, or_n1=or_n1, xor_n1=xor_n1)
+                swaps += s
+
+    return swaps
+
+
+def z_0(equations: dict[str, Equation]) -> list[tuple[str, str]]:
+    """x00-XOR-(z00)-y00"""
+    zn = "z00"
+    zn_equation = equations[zn]
+    zn_equation_expected = Equation(left="x00", op="XOR", right="y00")
+    if zn_equation == zn_equation_expected:
+        return []
+
+    print("z00 wrong")
+    real_zn = equation_to_name(equation=zn_equation_expected, equations=equations)
+    equations[zn], equations[real_zn] = equations[real_zn], equations[zn]
+    return [(zn, real_zn)]
+
+
+def z_1(equations: dict[str, Equation]) -> tuple[list[tuple[str, str]], str, str]:
+    """
+          ________XOR-(z01)________
+         /                         \
+    x00-AND-y00               x01-XOR-y01
+    """
+    swaps = []
+    zn = "z01"
+    zn_equation = equations[zn]
+    left_equation_expected = Equation(left="x00", op="AND", right="y00")
+    left_expected = equation_to_name(equation=left_equation_expected, equations=equations)
+    right_equation_expected = Equation(left="x01", op="XOR", right="y01")
+    right_expected = equation_to_name(equation=right_equation_expected, equations=equations)
+    zn_equation_expected = Equation(left=left_expected, op="XOR", right=right_expected)
+    if zn_equation != zn_equation_expected:
+        print("z01 wrong")
+        real_name = equation_to_name(equation=zn_equation_expected, equations=equations)
+        if real_name is not None:
+            swaps.append((zn, real_name))
+            equations[zn], equations[real_name] = equations[real_name], equations[zn]
+            return swaps, left_expected, right_expected
+
+        if zn_equation.op != zn_equation_expected.op:
+            raise Exception("z01 op wrong and cannot find replacement")
+
+        if zn_equation.left == left_expected:
+            expected_switch = (zn_equation.right, right_equation_expected)
+        elif zn_equation.left == right_expected:
+            expected_switch = (zn_equation.right, left_equation_expected)
+        elif zn_equation.right == right_expected:
+            expected_switch = (zn_equation.left, left_equation_expected)
+        elif zn_equation.right == left_expected:
+            expected_switch = (zn_equation.left, right_equation_expected)
+        else:
+            raise Exception("z01 left and right wrong and cannot find replacement")
+
+        real_name = equation_to_name(equation=expected_switch[1], equations=equations)
+        if real_name is None:
+            raise Exception(f"Failed to find replacement for {expected_switch[0]}")
+        old_name = expected_switch[0]
+        swaps.append((old_name, real_name))
+        equations[old_name], equations[real_name] = (equations[real_name], equations[old_name])
+
+    or_n1 = equation_to_name(equation=left_equation_expected, equations=equations)
+    xor_n1 = equation_to_name(equation=right_equation_expected, equations=equations)
+
+    return swaps, or_n1, xor_n1
+
+
+def z_n(n: int, equations: dict[str, Equation], or_n1: str, xor_n1: str) -> tuple[list[tuple[str, str]], str, str]:
+    """
+                          ____________________XOR-(Zn)________
+                         /                                     \
+             ________OR-(ORn)________                      Xn-XOR-(XORn)-Yn
+            /                        \
+    ORn1-AND-XORn1               Xn1-AND-Yn1
+    """
+    swaps = []
+    zn = f"z{n:02d}"
+    zn_equation = equations[zn]
+
+    or_n_left_equation_expected = Equation(left=or_n1, op="AND", right=xor_n1)
+    or_n_left_expected: str = equation_to_name(equation=or_n_left_equation_expected, equations=equations)
+
+    or_n_right_equation_expected = Equation(left=f"x{n-1:02d}", op="AND", right=f"y{n-1:02d}")
+    or_n_right_expected: str = equation_to_name(equation=or_n_right_equation_expected, equations=equations)
+
+    or_n_equation_expected = Equation(left=or_n_left_expected, op="OR", right=or_n_right_expected)
+    or_n_expected: str = equation_to_name(equation=or_n_equation_expected, equations=equations)
+
+    xor_n_equation_expected = Equation(left=f"x{n:02d}", op="XOR", right=f"y{n:02d}")
+    xor_n_expected: str = equation_to_name(equation=xor_n_equation_expected, equations=equations)
+
+    zn_equation_expected = Equation(left=or_n_expected, op="XOR", right=xor_n_expected)
+
+    # Case all ok
+    if zn_equation == zn_equation_expected:
+        return [], or_n_expected, xor_n_expected
+
+    print(f"{zn} wrong")
+    # Case zn swaps
+    real_name = equation_to_name(equation=zn_equation_expected, equations=equations)
+    if real_name is not None:
+        equations[zn], equations[real_name] = equations[real_name], equations[zn]
+        return [(zn, real_name)], or_n_expected, xor_n_expected
+
+    if zn_equation.op != zn_equation_expected.op:
+        raise Exception(f"{zn} op wrong and cannot find replacement")
+
+    # Case or_n and xor_n swaps
+    if zn_equation.left == or_n_expected:
+        expected_switch = (zn_equation.right, xor_n_equation_expected)
+    elif zn_equation.left == xor_n_expected:
+        expected_switch = (zn_equation.right, or_n_equation_expected)
+    elif zn_equation.right == xor_n_expected:
+        expected_switch = (zn_equation.left, or_n_equation_expected)
+    elif zn_equation.right == or_n_expected:
+        expected_switch = (zn_equation.left, xor_n_equation_expected)
+    else:
+        raise Exception(f"{zn} left and right wrong and cannot find replacement")
+
+    real_name = equation_to_name(equation=expected_switch[1], equations=equations)
+    # case or_n_left_expected or or_n_right_expected swaps
+    if real_name is None:
+        or_n_equation = equations[expected_switch[0]]
+        if or_n_equation.left == or_n_left_expected:
+            expected_switch = (or_n_equation.right, or_n_right_equation_expected)
+        elif or_n_equation.left == or_n_right_expected:
+            expected_switch = (or_n_equation.right, or_n_left_equation_expected)
+        elif or_n_equation.right == or_n_right_expected:
+            expected_switch = (or_n_equation.left, or_n_left_equation_expected)
+        elif or_n_equation.right == or_n_left_expected:
+            expected_switch = (or_n_equation.left, or_n_right_equation_expected)
+        else:
+            raise Exception(f"{zn} xor_n left and right wrong and cannot find replacement")
+
+    real_name = equation_to_name(equation=expected_switch[1], equations=equations)
+    old_name = expected_switch[0]
+    swaps.append((old_name, real_name))
+    equations[old_name], equations[real_name] = (equations[real_name], equations[old_name])
+
+    # After swaps
+    or_n_left_equation = Equation(left=or_n1, op="AND", right=xor_n1)
+    or_n_left: str = equation_to_name(equation=or_n_left_equation, equations=equations)
+
+    or_n_right_equation = Equation(left=f"x{n-1:02d}", op="AND", right=f"y{n-1:02d}")
+    or_n_right: str = equation_to_name(equation=or_n_right_equation, equations=equations)
+
+    or_n_equation = Equation(left=or_n_left, op="OR", right=or_n_right)
+    or_n: str = equation_to_name(equation=or_n_equation, equations=equations)
+
+    xor_n_equation = Equation(left=f"x{n:02d}", op="XOR", right=f"y{n:02d}")
+    xor_n: str = equation_to_name(equation=xor_n_equation, equations=equations)
+
+    return swaps, or_n, xor_n
+
+
+def equation_to_name(equation: Equation, equations: dict[str, Equation]) -> str | None:
+    for name, _equation in equations.items():
+        if equation == _equation:
+            return name
+    return None
 
 
 if __name__ == "__main__":
---
+++ 2024/24/main_2_visual.py
@@ -0,0 +1,136 @@
+class Equation:
+    def __init__(self, left: "Equation | str", right: "Equation | str", op: str, name: str):
+        self.raw_left = left
+        self.raw_right = right
+        self.op = op
+        self.name = name
+
+    def __eq__(self, other: "Equation"):
+        if not isinstance(other, Equation):
+            return False
+
+        if self.op != other.op:
+            return False
+
+        if (self.left == other.left and self.right == other.right) or (
+            self.left == other.right and self.right == other.left
+        ):
+            return True
+
+        # Case (a and b) or (c and d) or (e and f)
+        if (
+            isinstance(self.right, str)
+            or isinstance(self.left, str)
+            or isinstance(other.right, str)
+            or isinstance(other.left, str)
+        ):
+            return False
+
+        if not (self.right.op == self.left.op == other.right.op == other.left.op):
+            return False
+
+        return False
+
+    @property
+    def left(self):
+        if isinstance(self.raw_left, str) and not self.raw_left.startswith("x") and not self.raw_left.startswith("y"):
+            self.raw_left = EQUATIONS[self.raw_left]
+        return self.raw_left
+
+    @property
+    def right(self):
+        if (
+            isinstance(self.raw_right, str)
+            and not self.raw_right.startswith("x")
+            and not self.raw_right.startswith("y")
+        ):
+            self.raw_right = EQUATIONS[self.raw_right]
+        return self.raw_right
+
+    def __str__(self):
+        return f"Equation(left={self.left!r}, right={self.right!r}, op={self.op!r})"
+
+    def __repr__(self):
+        return str(self)
+
+    def display(self, depth: int | None = None):
+        lines, *_ = self._display_aux(depth)
+        for line in lines:
+            print(line)
+
+    def _display_aux(self, depth: int | None = None):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # Depth 0
+        if depth == 0:
+            left = self.left if isinstance(self.left, str) else self.left.name
+            right = self.right if isinstance(self.right, str) else self.right.name
+            line = f"{left}-{self.op}-({self.name})-{right}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # No child.
+        if isinstance(self.right, str) and isinstance(self.left, str):
+            line = f"{self.left}-{self.op}-({self.name})-{self.right}"
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if isinstance(self.right, str):
+            lines, n, p, x = self.left._display_aux(depth - 1 if depth is not None else None)
+            s = f"{self.op}-({self.name})-{self.right}"
+            u = len(s)
+            first_line = (x + 1) * " " + (n - x - 1) * "_" + s
+            second_line = x * " " + "/" + (n - x - 1 + u) * " "
+            shifted_lines = [line + u * " " for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if isinstance(self.left, str):
+            lines, n, p, x = self.right._display_aux(depth - 1 if depth is not None else None)
+            s = f"{self.left}-{self.op}-({self.name})"
+            u = len(s)
+            first_line = s + x * "_" + (n - x) * " "
+            second_line = (u + x) * " " + "\\" + (n - x - 1) * " "
+            shifted_lines = [u * " " + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux(depth - 1 if depth is not None else None)
+        right, m, q, y = self.right._display_aux(depth - 1 if depth is not None else None)
+        s = f"{self.op}-({self.name})"
+        u = len(s)
+        first_line = (x + 1) * " " + (n - x - 1) * "_" + s + y * "_" + (m - y) * " "
+        second_line = x * " " + "/" + (n - x - 1 + u + y) * " " + "\\" + (m - y - 1) * " "
+        if p < q:
+            left += [n * " "] * (q - p)
+        elif q < p:
+            right += [m * " "] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * " " + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+EQUATIONS: dict[str, Equation] = {}
+
+
+def main():
+    with open("input", "r") as f:
+        content: str = f.read()
+
+    _, equations_raw = content.split("\n\n")
+
+    for equation in equations_raw.splitlines():
+        n1, operation, n2, _, r = equation.split(" ")
+        EQUATIONS[r] = Equation(left=n1, right=n2, op=operation, name=r)
+
+    for n in range(4):
+        print(f"---- z{n:02d} -----")
+        EQUATIONS[f"z{n:02d}"].display(depth=2)
+
+
+if __name__ == "__main__":
+    main()
